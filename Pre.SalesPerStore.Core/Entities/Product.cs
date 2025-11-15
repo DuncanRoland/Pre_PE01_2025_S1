a38d02847e2068b773988058ffa18c6a2d2bc34d
Duncan Roland <duncan.roland@student.howest.be>
2025-11-15 22:19:50 +0100

    Make the entities and appropriate properties

---
+++ Pre.SalesPerStore.Core/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Pre.SalesPerStore.Core.Entities;
+
+public class Product
+{
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal SellPrice { get; set; }
+    public decimal BuyingPrice { get; set; }
+}
---
+++ Pre.SalesPerStore.Core/Entities/Store.cs
@@ -0,0 +1,9 @@
+﻿namespace Pre.SalesPerStore.Core.Entities;
+
+public class Store
+{
+    public string StoreName { get; set; }
+    public string StoreCountry { get; set; }
+    public DateTime EstablishedDate { get; set; }
+    public List<Product> Products { get; set; }
+}
